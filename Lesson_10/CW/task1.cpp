d11636c676580f56154de63522176454eb0ef7c9
Xenia Ruban <31805634+XeniaRuban@users.noreply.github.com>
2017-11-25 11:34:48 +0300

    Create task1.cpp

---
+++ Lesson_10/CW/task1.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include <stdlib.h>
+int main()
+{
+	FILE *file;
+	char string[100];
+	int value = 0;
+	int sum_value = 0;
+	errno_t err = fopen_s(&file, "C:\\Users\\uc2\\Desktop\\file.txt", "r");
+	if (err)
+		printf_s("The file file.txt was not opened\n");
+	else
+	{
+		while (fscanf_s(file, "%100s %d", string, 100, &value) != EOF) {
+			printf("%s ", string);
+			printf("%d\n", value);
+			sum_value = sum_value + value;
+		} printf("%d\n", sum_value);
+	}
+	fclose(file);
+	return 0;
+}
