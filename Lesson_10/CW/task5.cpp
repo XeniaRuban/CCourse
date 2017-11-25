af3f459c1f1d78e73f26701b81037519b602056c
Xenia Ruban <31805634+XeniaRuban@users.noreply.github.com>
2017-11-25 13:18:56 +0300

    Create task5.cpp

---
+++ Lesson_10/CW/task5.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include <string.h>
+#define BUFFER_SIZE 1024
+int main()
+{
+	FILE *file;
+	char string_first[BUFFER_SIZE];
+	char string_second[BUFFER_SIZE] = "Mick";
+
+	errno_t err = fopen_s(&file, "C:\\Users\\uc2\\Desktop\\file.txt", "r");
+	if (err)
+		printf_s("The file file.txt was not opened\n");
+	else
+		while (fgets(string_first, BUFFER_SIZE, file) != NULL) {
+			if(strstr(string_first, string_second) != 0)
+				printf("%s\n", string_first);
+		}
+	return 0;
+}
