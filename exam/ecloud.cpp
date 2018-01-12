b4df3e417930f5dd2d2da6109e96421d289a4d04
Xenia Ruban <31805634+XeniaRuban@users.noreply.github.com>
2018-01-12 12:27:39 +0300

    Create ecloud.cpp

---
+++ exam/ecloud.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+#include <math.h>
+
+int main()
+{
+	float X0, Y0, R, X, Y = 0;
+	printf("X0: ");
+	scanf_s("%f", &X0);
+	printf("Y0: ");
+	scanf_s("%f", &Y0);
+	printf("R: ");
+	scanf_s("%f", &R);
+
+	if (R > 0)
+	{
+		X = X0 - R;
+		Y = Y0;
+		printf("X: %f\n", X);
+		printf("Y: %f\n", Y);
+		while (X < X0 + R)
+		{
+			X = X + 0.1;
+			printf("X: %f\n", X);
+			Y = sqrt(R*R - (X - X0)*(X - X0)) + Y0;
+			printf("Y: %f\n", Y);
+			printf("X: %f\n", X);
+			Y = - sqrt(R*R - (X - X0)*(X - X0)) + Y0;
+			printf("Y: %f\n", Y);
+		}
+		X = X0 + R;
+		Y = Y0;
+		printf("X: %f\n", X);
+		printf("Y: %f\n", Y);
+
+	}
+	else
+		printf("Error!\n");
+
+	return 0;
+}
