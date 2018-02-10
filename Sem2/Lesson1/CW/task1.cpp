00b769ae95e6a2ff960917c5307c52f7dcd0b3ca
Xenia Ruban <31805634+XeniaRuban@users.noreply.github.com>
2018-02-10 13:54:31 +0300

    Create task1.cpp

---
+++ Sem2/Lesson1/CW/task1.cpp
@@ -0,0 +1,66 @@
+#include "stdafx.h"
+#include <stdlib.h>
+#include <string.h>
+#define BUFFER_SIZE 1024
+
+int main()
+{
+	FILE *file;
+	char buffer[BUFFER_SIZE];
+	errno_t err = fopen_s(&file, "C:\\Users\\uc2\\Desktop\\transactions.txt", "r");
+	if (err)
+		printf_s("The file wasn't opened\n");
+	else
+	{
+
+	float average_price = 0;
+	int n = 0;
+	int max_number = 0;
+    int min_number = 1000;
+		while (fgets(buffer, BUFFER_SIZE, file) != NULL) 
+		{
+			printf("%s", buffer);
+			int count = 0;
+			int price = 0;
+			int number = 1;
+			for (int i = 0; i < strlen(buffer); i++) 
+			{
+				if (buffer[i] == ';') 
+				{
+					printf_s("Position: %d\n", i);
+					count++;
+				}
+				if (count == 1 && buffer[i] != ';') 
+				{
+					price = price * 10 + (buffer[i] - 48);
+		        }
+				if (buffer[i] == ',')
+				{ 
+					number = number++;
+					count++;
+				}
+
+			}
+			average_price = average_price + price;
+			printf_s("Price: %d\n", price);
+			n++;
+			printf_s("Number: %d\n", number);
+
+			if (number > max_number)
+				max_number = number;
+	
+			if (number < min_number)
+				min_number = number;
+
+		} 
+		average_price = average_price/n;
+		printf_s("Average price: %f\n", average_price);
+		    
+			printf_s("Max_number: %d\n", max_number);
+			printf_s("Min_number: %d\n", min_number);
+
+	}
+
+	fclose(file);
+	return 0;
+}
