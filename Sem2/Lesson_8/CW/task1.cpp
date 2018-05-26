e2cc6806ad114bc45ebae8707bfc21867e41bc0a
Xenia Ruban <31805634+XeniaRuban@users.noreply.github.com>
2018-05-26 09:54:07 +0300

    Create task1.cpp

---
+++ Sem2/Lesson_8/CW/task1.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+
+class Worker {
+public:
+	// Создаём виртуальную функцию
+	virtual int process(int *, int) = 0;
+};
+
+//Поиск минимума
+class MinFinder : public Worker {
+	int process(int *data, int size) {
+		int result = 1000;
+		for (int i = 0; i < size; i++) {
+			if (data[i] < result)
+				result = data[i];
+		}
+		return result;
+	}
+};
+
+//Поиск максимума
+class MaxFinder : public Worker {
+	int process(int *data, int size) {
+		int result = -1000;
+		for (int i = 0; i < size; i++) {
+			if (data[i] > result)
+				result = data[i];
+		}
+		return result;
+	}
+};
+
+int main()
+{
+	Worker *myWorker; // Указатель на базовый класс!!!
+	myWorker = new MaxFinder();
+	//myWorker = new MinFinder();
+	int data[5] = { 1,7,3,8,2 };
+	// Полиморфное поведение
+	printf("Result: %d\n", myWorker->process(data, 5));
+	return 0;
+}
